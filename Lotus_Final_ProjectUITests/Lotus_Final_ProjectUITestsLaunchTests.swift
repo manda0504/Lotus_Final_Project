aba015689d4a10f8b831681de7029ba166678c5b
Amanda Alem <a.alem0504@gmail.com>
2022-07-19 13:20:23 -0400

    Initial Commit

---
+++ Lotus_Final_ProjectUITests/Lotus_Final_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lotus_Final_ProjectUITestsLaunchTests.swift
+//  Lotus_Final_ProjectUITests
+//
+//  Created by scholar on 7/19/22.
+//
+
+import XCTest
+
+class Lotus_Final_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
