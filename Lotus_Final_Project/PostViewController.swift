7acebad76100f6fb7e423ed4728d4ae5256aedff
Amanda Alem <a.alem0504@gmail.com>
2022-07-21 14:37:12 -0400

    created another a view controller

--- Lotus_Final_Project/PostViewController.swift
+++ Lotus_Final_Project/PostViewController.swift
@@ -9,25 +9,15 @@ import UIKit
 
 class PostViewController: UIViewController {
 
-    @IBAction func selectImageButton(_ sender: UIButton) {
-    }
-    @IBOutlet weak var pictureSelected: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
